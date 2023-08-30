93a89b37beb949bc5d656d50623de9238dc145c4
Artem Egorov <artegtor@gmail.com>
2023-08-30 12:32:41 +0500

    initial project

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import { BootstrapVue } from "bootstrap-vue";
+import "./assets/main.scss";
+import App from "./App.vue";
+
+createApp(App).use(BootstrapVue).mount("#app");
